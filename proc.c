c5a728eb602e21f98af97043348820db644d858b
l-herr <lherr000@gmail.com>
2025-11-02 18:55:17 -0500

    Update proc.c

--- proc.c
+++ proc.c
@@ -368,6 +368,26 @@ scheduler(void)
     release(&ptable.lock);
   }
 }
+
+void
+sched(void)
+{
+  int intena;
+  struct proc *p = myproc();
+
+  if(!holding(&ptable.lock))
+    panic("sched ptable.lock");
+  if(mycpu()->ncli != 1)
+    panic("sched locks");
+  if(p->state == RUNNING)
+    panic("sched running");
+  if(readeflags() & FL_IF)
+    panic("sched interruptible");
+  intena = mycpu()->intena;
+  swtch(&p->context, mycpu()->scheduler);
+  mycpu()->intena = intena;
+}
+
 // Give up the CPU for one scheduling round.
 void
 yield(void)
